f50fa3954fbacfe2a0815b1419c51efeb32aa6ce
José Mata <josemata@Joses-MacBook-Pro.local>
2024-11-27 19:59:46 -0500

    finalized the implementation of all features except the feature to delete a phonebook entry and the feature to handle hashmap collisions

--- phone_directory.py
+++ phone_directory.py
@@ -31,15 +31,29 @@
 class TrieNode:
     def __init__(self):
         self.child = [None] * 27 #Creates a list of 27 indexes containing the object None: 26 letters of the English alphabet and an additional index for spaces. 
-        self.wordEnd = False
+        self.word_end = False
 
-root = TrieNode()
+root = TrieNode() #the root node for the trie data structure is one that always starts completely empty (no characters in its children and the wordEnd attribute is False)
+
+hashmap = [None]*100 #The hashmap consists of a list of 100 items (indexes 0 to 99) that is initialized with all-zero values 
+
+#This is a hash function that simply calculates the sum of the equivalent ascii value of each character, then divides it by a hundred, and returns the remainder as the hash code (index)
+def hash_function(name):
+    #Convert the name to only lowercase letters
+    name = name.lower()
+    #Remove leading and trailing whitespace characters
+    name = name.strip()
+    total_value_of_name = 0
+    for character in name:
+        total_value_of_name = total_value_of_name + ord(character)
+    index = total_value_of_name%100 #returns a value from 0 to 99 which is the remainder of the sum of all ascii equivalents divided by 100
+    return index
+
 
 #Part of the code for function "trie_add" was obtained from https://www.geeksforgeeks.org/trie-insert-and-search/ and modified to fit the needs of this program 
 #This is the function that adds an name entry to the trie data structure
 def trie_add(root, name):
-    print("Executing trie_add function in the name: ", name)
-    #Covert the name to use only lowercase letters
+    #Convert the name to only lowercase letters
     name = name.lower()
     #Remove leading and trailing whitespace characters
     name = name.strip()
@@ -62,9 +76,10 @@ def trie_add(root, name):
     # Mark the end of the word
     curr.word_end = True
 
-def hasmap_add():
-    #This is the function that adds an entry to the hashmap data structure
-    print("hashmap_add function sucessfully executed")
+#This is the function that adds an entry to the hashmap data structure
+def hashmap_add(name, number):
+    index = hash_function(name)
+    hashmap[index] = (name, number)
 
 #Part of the code for function "trie_lookup" was obtained from https://www.geeksforgeeks.org/trie-insert-and-search/ and modified to fit the needs of this program 
 #This is the implementation of the Trie data structure to check if a name is already included in the phonebook. 
@@ -89,19 +104,54 @@ def trie_lookup(root, name):
     # Return true if the word exists and is marked as ending
     return curr.word_end
 
-def hashmap_lookup():
-    #This is the manual implementation of a hash map to store the saved names. 
-    print("hashmap_ lookup function successfully executed")
+#This is the manual implementation of a hash map to retrieve the saved names and numbers. 
+def hashmap_lookup(name):
+    index = hash_function(name)
+    name_and_number = hashmap[index]
+    return name_and_number
 
+#This is the function that looks for a person in the phonebook directory.
+#First, it uses the trie data structure to see if the name exists in the phonebook directory. If it does, then it retrieves it in the hashmap along with the phone number.
+def option_1():
+    user_answer_1 = input("Enter the name of the person that you're looking for in the phonebook directory. Type their first name, then a space, and then their last name.\n")
+    found_status = trie_lookup(root, user_answer_1)
+    if found_status == True:
+        print("Name " + user_answer_1 + " was found in the phonebook directory.")
+        name_and_number = hashmap_lookup(user_answer_1)
+        print("Name: " + name_and_number[0] + "\nNumber: " + name_and_number [1] + "\n\n")
+    else:
+        print("Name " + user_answer_1 + " was NOT found in the phonebook directory.\n\n")
+
+def option_2(root):
+    user_name = input("Please enter the person's first name, then a space, and then their last name.\n")
+    user_number = input("Please enter the person's phone number in the following format: xxx-xxx-xxxx\n")
+    trie_add(root, user_name)
+    hashmap_add(user_name, user_number)
+    print("User '" + user_name +"' has been succesfully added to the phonebook directory.\n\n")
+
+def option_3(hashmap):
+    print("Here is a list of all the names and numbers in the phonebook directory:")
+    for pair in hashmap:
+        if pair != None:
+            print(pair)
+    print("\n\n")
 dummy_data = [('Arnold Mpiima', '123-456-7890'), ('Lalitha Bhavanand', '123-123-1231'), ('Jose Mata', '504-123-4567')]
 
 for name, number in dummy_data:
     trie_add(root, name)
+    hashmap_add(name, number)
 
-user_answer = input("Enter the name of the person that you're looking for in the phonebook directory.\n")
-found_status = trie_lookup(root, user_answer)
-if found_status == True:
-    print("Name " + user_answer + " was found.")
-else:
-    print("Name " + user_answer + " was NOT found.")
-
+print("Welcome to our group's phone directory simulator using hashmap and trie data structures.")
+while True: 
+    user_answer_0 = input("Please choose one of the four options below:\nEnter '1' to look for a person in the phonebook directory.\nEnter '2' to add a new person to the phonebook directory.\nEnter '3' to display all the names and numbers in the phonebook.\nEnter '4' to end the program.\n")
+    if user_answer_0 == '1':
+        option_1()
+    elif user_answer_0 == '2':
+        option_2(root)
+    elif user_answer_0 == '3':
+        option_3(hashmap)
+    elif user_answer_0 == '4':
+        print("Exiting program...")
+        break
+    else:
+        print("Answer is invalid")
